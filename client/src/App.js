6c0f70aad08bf8da56cae5b80fbb6fc3939106f0
Cindy1229 <leticiashi3@gmail.com>
2020-09-20 22:45:14 -0400

    Create profile page

--- client/src/App.js
+++ client/src/App.js
@@ -3,9 +3,11 @@ import CreateAccount from "./components/CreateAccount";
 import Login from "./components/Login";
 import Home from "./components/Home";
 import ForgotPassword from "./components/ForgotPassword";
+import Profile from "./components/Profile";
 import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
 
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   const loggedIn = false;
@@ -16,6 +18,7 @@ function App() {
         <Route path="/login" component={Login} />
         <Route path="/createaccount" component={CreateAccount}></Route>
         <Route path="/forgotpass" component={ForgotPassword}></Route>
+        <Route path="/profile" component={Profile}></Route>
         <Route exact path="/">
           {loggedIn ? <Redirect to="/home" /> : <Redirect to="/login" />}
         </Route>
