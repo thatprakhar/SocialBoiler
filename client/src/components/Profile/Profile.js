f5c07a7981fa479178649fb2763645339704bbe3
Cindy1229 <leticiashi3@gmail.com>
2020-09-29 12:19:57 -0400

    merged localstorage with prakhar

--- client/src/components/Profile/Profile.js
+++ client/src/components/Profile/Profile.js
@@ -5,7 +5,6 @@ import ProfileHeader from "./ProfileHeader";
 import ProfileInfo from "./ProfileInfo";
 import queryString from "query-string";
 import { useLocation } from "react-router-dom";
-import Consumer from "../configContext";
 
 import "./Profile.css";
 
@@ -20,47 +19,35 @@ function Profile() {
   // console.log(query);
 
   return (
-    <Consumer>
-      {(context) => {
-        //console.log("here", localStorage.getItem("userLoggedIn"));
-        return localStorage.getItem("userLoggedIn") == "true" ? (
-          <div className="profile">
-            <ProfileHeader />
-            <Container fluid>
-              <Row>
-                <Col md={4}>
-                  <ProfileCard
-                    handleShow={handleShow}
-                    isOwnProfile={isOwnProfile}
-                  />
-                </Col>
+    <div className="profile">
+      <ProfileHeader />
+      <Container fluid>
+        <Row>
+          <Col md={4}>
+            <ProfileCard handleShow={handleShow} isOwnProfile={isOwnProfile} />
+          </Col>
 
-                <Col md={8}>
-                  <ProfileInfo isOwnProfile={isOwnProfile} />
-                </Col>
-              </Row>
-            </Container>
+          <Col md={8}>
+            <ProfileInfo isOwnProfile={isOwnProfile} />
+          </Col>
+        </Row>
+      </Container>
 
-            <Modal show={show} onHide={handleClose}>
-              <Modal.Header closeButton>
-                <Modal.Title>Delete Account</Modal.Title>
-              </Modal.Header>
-              <Modal.Body>Are you sure to delete your account?</Modal.Body>
-              <Modal.Footer>
-                <Button variant="secondary" onClick={handleClose}>
-                  Close
-                </Button>
-                <Button variant="danger" onClick={handleClose}>
-                  Delete
-                </Button>
-              </Modal.Footer>
-            </Modal>
-          </div>
-        ) : (
-          <button onClick={context.toggleLogin}>login</button>
-        );
-      }}
-    </Consumer>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Delete Account</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Are you sure to delete your account?</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="danger" onClick={handleClose}>
+            Delete
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </div>
   );
 }
 
--- client/src/components/Profile/ProfileHeader.js
+++ client/src/components/Profile/ProfileHeader.js
@@ -1,30 +1,29 @@
 import React from "react";
 import { Nav, Navbar, Button } from "react-bootstrap";
-import Consumer from "../configContext";
+import { useHistory } from "react-router-dom";
 
 import "./Profile.css";
 
 function ProfileHeader() {
+  const history = useHistory();
+  const logout = () => {
+    localStorage.removeItem("user");
+    history.push("/login");
+  };
   return (
-    <Consumer>
-      {(context) => {
-        return (
-          <Navbar bg="dark" expand="lg" variant="dark" sticky="top">
-            <Navbar.Brand href="/home">SocialBoiler</Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-              <Nav className="ml-auto">
-                <Nav.Link href="/">Home</Nav.Link>
-                <Nav.Link href="#link">Posts</Nav.Link>
-                <Button variant="info" onClick={context.toggleLogin}>
-                  Logout
-                </Button>
-              </Nav>
-            </Navbar.Collapse>
-          </Navbar>
-        );
-      }}
-    </Consumer>
+    <Navbar bg="dark" expand="lg" variant="dark" sticky="top">
+      <Navbar.Brand href="/home">SocialBoiler</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="ml-auto">
+          <Nav.Link href="/">Home</Nav.Link>
+          <Nav.Link href="#link">Posts</Nav.Link>
+          <Button variant="info" onClick={logout}>
+            Logout
+          </Button>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
   );
 }
 
