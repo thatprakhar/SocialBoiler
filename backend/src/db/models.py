d319e55cbc10e00b4afd1d1f55653c71d3809ab2
onuronen <oonen@purdue.edu>
2020-10-12 17:46:35 -0400

    New tables added

--- backend/src/db/models.py
+++ backend/src/db/models.py
@@ -21,16 +21,36 @@ class User_Credentials(Base):
     email = Column(String, nullable=False)
     password = Column(String, nullable=False)
     auth_token = Column(String, nullable=True)
+    followers = Column(String, nullable=True)
+    following = Column(String, nullable=True)
+    topics_following = Column(String, nullable=True)
     
 
 class Profile_Page(Base):
     __tablename__ = "profile_page"
-    username =Column (String, primary_key=True, nullable=False)
+    username = Column(String, primary_key=True, nullable=False)
     email = Column(String, nullable=False)
     phone_number = Column(String, nullable=True)
     age = Column(String, nullable=True)
     about = Column(String, nullable=True)
     image=Column(String, nullable=True)
+
+
+class Posts(Base):
+    __tablename__ = "posts"
+    username = Column(String, primary_key=True, nullable=False)
+    post_id = Column(Integer, autoincrement=True)
+    likes = Column(Integer, nullable=True)
+    dislikes = Column(Integer, nullable=True)
+    date_created = Column(Integer, nullable=True)
+    topics = Column(String, nullable=True)
+
+
+class Comments(Base):
+    __tablename__ = "comments"
+    username = Column(String, primary_key=True, nullable=False)
+    post_id = Column(Integer, nullable=False)
+    comment = Column(String, nullable=False)
 
 
 if __name__ == "__main__":
