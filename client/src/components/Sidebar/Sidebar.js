28d724011b0d02faa0eb6020e0533cd97e4058a0
thatprakhar <pnahar@purdue.edu>
2020-10-27 00:18:07 +0530

    UI improvements

--- client/src/components/Sidebar/Sidebar.js
+++ client/src/components/Sidebar/Sidebar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Typography,
   makeStyles,
@@ -6,9 +6,14 @@ import {
   ListItem,
   ListItemAvatar,
   ListItemText,
-  Avatar
+  Avatar,
+  Snackbar
 } from "@material-ui/core";
-import { Button, Badge } from "react-bootstrap";
+import { Button, Badge, Alert } from "react-bootstrap";
+import Backdrop from "@material-ui/core/Backdrop";
+import CircularProgress from "@material-ui/core/CircularProgress";
+
+
 
 const createStyles = makeStyles(theme => ({
   inline: {
@@ -25,18 +30,56 @@ const createStyles = makeStyles(theme => ({
     backgroundColor: theme.palette.background.paper,
     maxHeight: window.innerHeight,
     maxWidth: "50ch"
+  },
+  backdrop: {
+    zIndex: theme.zIndex.drawer + 1,
+    color: "#fff"
+  },
+  notif: {
+    zIndex: theme.zIndex.drawer + 1,
+    position: "absolute"
   }
 }));
 
 export default function App(props) {
   var styling = createStyles();
+  const [loading, setLoading] = useState(false);
+  const [showSuccessFollow, setShowSuccessFollow] = useState(false);
+  const [showError, setShowError] = useState(false);
+  
+
+  const API_URL = "http://127.0.0.1:5000";
 
   function parentHandle(post_data) {
     props.parentHandler(post_data);
   }
 
   function followTopic() {
-    
+    setLoading(true);
+    const requestOptions = {
+      method: "POSt",
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+        username: localStorage.getItem("username"),
+        auth_token: localStorage.getItem("auth_token"),
+        topic: props.topic
+      }
+    };
+    fetch(API_URL + "/follow_topic", requestOptions)
+    .then(res => res.json())
+    .then(data => {
+      console.log(data);
+      if (data === "failed") {
+        setShowError(true); 
+      } else {
+        setShowSuccessFollow(true);
+      }
+      setLoading(false);
+    })
+    .catch(err => {
+      console.log(err)
+      setLoading(false);
+    });
   }
 
   function post_view(post_data) {
@@ -81,13 +124,28 @@ export default function App(props) {
   }
 
   var posts = props.posts.map(x => <li key={x.post_id}>{post_view(x)}</li>);
-return <List className={styling.root}>
-  {
-  props.page_type === "search_posts" ?
-    <li><Typography variant="h5">{topic_view(props.topic)}</Typography></li>
-  : 
-    null
-  }
-  {posts}
-  </List>;
+return <>
+    <Snackbar open={showSuccessFollow} autoHideDuration={6000} onClose={() => setShowSuccessFollow(!showSuccessFollow)}>
+      <Alert onClose={() => setShowSuccessFollow(!showSuccessFollow)} variant="success" dismissible>
+        Now following {props.topic}
+      </Alert>
+    </Snackbar>
+    <Snackbar open={showError} autoHideDuration={6000} onClose={() => setShowError(!showError)}>
+      <Alert onClose={() => setShowError(!showError)} variant="danger" dismissible>
+        An error occured
+      </Alert>
+      </Snackbar>
+      <Backdrop className={styling.backdrop} open={loading}>
+        <CircularProgress color="inherit" />
+      </Backdrop>
+      <List className={styling.root}>
+        {
+          props.page_type === "search_posts" ?
+          <li><Typography variant="h5">{topic_view(props.topic)}</Typography></li>
+          : 
+          null
+        }
+      {posts}
+      </List>
+  </>
 }
